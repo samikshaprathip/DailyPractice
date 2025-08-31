25a589fc639403ea8197a323d19a30dcc617c911
samiksha <samikshas503@gmail.com>
2025-08-31 19:00:17 +0530

    Updated files

---
+++ src/Soduku.java
@@ -0,0 +1,105 @@
+import java.util.*;
+
+public class Soduku {
+    int n = 3;
+    int N = n * n;
+    int[][] rows = new int[N][N + 1];
+    int[][] columns = new int[N][N + 1];
+    int[][] boxes = new int[N][N + 1];
+    char[][] board;
+    boolean sudokuSolved = false;
+
+    public boolean couldPlace(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        return rows[row][d] + columns[col][d] + boxes[idx][d] == 0;
+    }
+
+    public void placeNumber(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        rows[row][d]++;
+        columns[col][d]++;
+        boxes[idx][d]++;
+        board[row][col] = (char)(d + '0');
+    }
+
+    public void removeNumber(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        rows[row][d]--;
+        columns[col][d]--;
+        boxes[idx][d]--;
+        board[row][col] = '.';
+    }
+
+    public void placeNextNumbers(int row, int col) {
+        if (row == N - 1 && col == N - 1) {
+            sudokuSolved = true;
+        } else if (col == N - 1) {
+            backtrack(row + 1, 0);
+        } else {
+            backtrack(row, col + 1);
+        }
+    }
+
+    public void backtrack(int row, int col) {
+        if (board[row][col] == '.') {
+            for (int d = 1; d <= 9; d++) {
+                if (couldPlace(d, row, col)) {
+                    placeNumber(d, row, col);
+                    placeNextNumbers(row, col);
+                    if (!sudokuSolved) {
+                        removeNumber(d, row, col);
+                    }
+                }
+            }
+        } else {
+            placeNextNumbers(row, col);
+        }
+    }
+
+    public void solveSudoku(char[][] board) {
+        this.board = board;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    placeNumber(Character.getNumericValue(num), i, j);
+                }
+            }
+        }
+        backtrack(0, 0);
+    }
+
+    // Utility function to print the board
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        Soduku solver = new Soduku();
+        System.out.println("Sudoku puzzle:");
+        printBoard(board);
+
+        solver.solveSudoku(board);
+
+        System.out.println("\nSolved Sudoku:");
+        printBoard(board);
+    }
+}
