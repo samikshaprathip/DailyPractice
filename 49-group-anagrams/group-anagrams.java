c99f67a7b58886c300d4221c49bb529023add558
samikshaprathip <samikshas503@gmail.com>
2025-12-16 08:06:09 +0530

    Time: 18 ms (25.87%) | Memory: 51.1 MB (6.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap <String,List<String>> res= new HashMap<>();
+        HashMap<String,List<String>> res = new HashMap<>();
         for(String s : strs){
             int[] count = new int[26];
             for(char c : s.toCharArray()){
