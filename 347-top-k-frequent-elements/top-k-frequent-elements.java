101e9eae8256cbe24143bedd2b0a8d6b74f8c7e6
samikshaprathip <samikshas503@gmail.com>
2025-12-17 17:48:25 +0530

    Time: 12 ms (84.24%) | Memory: 51.7 MB (5.36%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,8 +1,8 @@
-class Solution{
-    public int[] topKFrequent(int[] nums,int k){
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> count = new HashMap<>();
-        List<Integer>[] freq = new List[nums.length + 1];
-        for(int i =0;i<freq.length;i++){
+        List<Integer>[] freq = new List[nums.length+1];
+        for(int i=0;i<freq.length;i++){
             freq[i] = new ArrayList<>();
         }
         for(int n : nums){
@@ -12,11 +12,11 @@ class Solution{
             freq[entry.getValue()].add(entry.getKey());
         }
         int[] res = new int[k];
-        int index = 0;
-        for(int i = freq.length-1;i>0 && index<k;i--){
+        int index=0;
+        for(int i=freq.length-1; i>0 && index<k; i--){
             for(int n : freq[i]){
                 res[index++]=n;
-                if(index==k){
+                if(index == k){
                     return res;
                 }
             }
