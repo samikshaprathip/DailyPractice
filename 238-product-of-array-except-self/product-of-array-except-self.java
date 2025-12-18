457b98bdb46feded33efa79986a6b3fe5fc95d48
samikshaprathip <samikshas503@gmail.com>
2025-12-18 07:39:51 +0530

    Time: 2 ms (92.67%) | Memory: 72.1 MB (28.34%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -3,11 +3,11 @@ class Solution {
         int n = nums.length;
         int[] res = new int[n];
         res[0]=1;
-        for(int i =1;i<n;i++){
+        for(int i=1;i<n;i++){
             res[i] = res[i-1] * nums[i-1];
         }
         int postfix = 1;
-        for(int i = n-1;i>=0;i--){
+        for(int i=nums.length-1;i>=0;i--){
             res[i] *= postfix;
             postfix *= nums[i];
         }
