5f35fda28941467de41aacc14539d4ae04c3bea9
samikshaprathip <samikshas503@gmail.com>
2025-12-19 17:32:06 +0530

    Time: 9 ms (45.93%) | Memory: 44.2 MB (43.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -3,33 +3,33 @@ class Solution {
         if(s1.length() > s2.length()){
             return false;
         }
-        int[] s1c = new int[26];
+        int[] s1c= new int[26];
         int[] s2c = new int[26];
         for(int i=0;i<s1.length();i++){
             s1c[s1.charAt(i) - 'a']++;
             s2c[s2.charAt(i) - 'a']++;
-        } 
+        }
         int matches = 0;
         for(int i=0;i<26;i++){
             if(s1c[i] == s2c[i]){
                 matches++;
             }
         }
-        int l = 0;
+        int l=0;
         for(int r=s1.length();r<s2.length();r++){
             if(matches == 26){
                 return true;
             }
             int index = s2.charAt(r)-'a';
             s2c[index]++;
-            if(s1c[index]==s2c[index]){
+            if(s1c[index] == s2c[index]){
                 matches++;
             }else if(s1c[index]+1 == s2c[index]){
                 matches--;
             }
             index = s2.charAt(l)-'a';
             s2c[index]--;
-            if(s1c[index]==s2c[index]){
+            if(s1c[index] == s2c[index]){
                 matches++;
             }else if(s1c[index]-1 == s2c[index]){
                 matches--;
