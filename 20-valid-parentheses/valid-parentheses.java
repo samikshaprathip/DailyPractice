851c360137a5398019177ebbcfd5ddf3ef490b52
samikshaprathip <samikshas503@gmail.com>
2026-01-27 07:25:47 +0530

    Time: 199 ms (6.68%) | Memory: 46.8 MB (5.11%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,9 +1,9 @@
-class Solution {
-    public boolean isValid(String s) {
-        while(s.contains("()") || s.contains("[]") || s.contains("{}")){
-            s = s.replace("[]","");
-            s = s.replace("{}","");
-            s = s.replace("()","");
+class Solution{
+    public boolean isValid(String s){
+        while(s.contains("[]") || s.contains("{}") || s.contains("()")){
+            s=s.replace("()","");
+            s=s.replace("{}","");
+            s=s.replace("[]","");
         }
         return s.isEmpty();
     }
