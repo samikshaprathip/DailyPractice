6b0f5d0378440fd7394586829a3b50ba0b5a3520
samikshaprathip <samikshas503@gmail.com>
2026-01-28 08:02:06 +0530

    Time: 105 ms (16.33%) | Memory: 106 MB (24.72%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -8,8 +8,8 @@ class Solution {
         Arrays.sort(pair,(a,b) -> Integer.compare(b[0],a[0]));
         Stack<Double> stack = new Stack<>();
         for(int[] p : pair){
-            stack.push((double)(target - p[0])/p[1]);
-            if(stack.size()>=2 && stack.peek() <= stack.get(stack.size()-2)){
+            stack.push((double)(target-p[0])/p[1]);
+            if(stack.size() >= 2 && stack.peek() <= stack.get(stack.size()-2)){
                 stack.pop();
             }
         }
