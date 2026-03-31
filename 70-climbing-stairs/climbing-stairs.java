695078951c3d7ca0d55a2e70afcb6d42a18a0390
samikshaprathip <samikshas503@gmail.com>
2026-03-31 19:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (80.85%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,12 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=2) return n;
-        int a =1;
-        int b=2;
-        for(int i =3;i<=n;i++){
-            int c = a+b;
-            a = b;
-            b = c;
+        if(n<=2)return n;
+        int a=1,b=2;
+        for(int i=3;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
         }
         return b;
     }
